db5fc01c3e9cc34310a4129d8641831d86f02a86
MG <m.guerid@310k.ch>
2022-11-18 22:23:38 +0100

    Add Luhn class helper

---
+++ src/Luhn.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace K310\Helpers;
+
+use Illuminate\Support\Traits\Macroable;
+
+class Luhn
+{
+    use Macroable;
+
+    /**
+     * Returns the given number with luhn algorithm applied.
+     *
+     * For example 456 becomes 4564.
+     *
+     * @param  integer $number
+     * @return integer
+     */
+    public function generate($number)
+    {
+        $stack = 0;
+        $digits = str_split(strrev($number), 1);
+
+        foreach($digits as $key => $value)
+        {
+            if($key % 2 === 0)
+            {
+                $value = array_sum(str_split($value * 2, 1));
+            }
+
+            $stack += $value;
+        }
+
+        $stack %= 10;
+
+        if($stack !== 0)
+        {
+            $stack -= 10;
+        }
+
+        return (int) (implode('', array_reverse($digits)) . abs($stack));
+    }
+
+    /**
+     * Validates the given number.
+     *
+     * @param  integer $number
+     * @return boolean
+     */
+    public function validate($number)
+    {
+        $original = substr($number, 0, strlen($number) - 1);
+
+        return $this->generate($original) === $number;
+    }
+}
